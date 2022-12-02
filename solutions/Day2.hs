ee0616b8d9ffd3a17dd80527b67e39f5cc103eff
ivy bridges <ivy.v.bridges@gmail.com>
2022-12-02 01:45:59 -0500

    refactored day 2 solution

--- solutions/Day2.hs
+++ solutions/Day2.hs
@@ -1,70 +1,71 @@
 module Day2 where
 
 data Shape = Rock | Paper | Scissors
-    deriving (Read, Show, Eq)
+    deriving (Read, Show, Eq, Ord)
 
-
-result :: Shape -> Shape -> Int
-result Rock Scissors = 6
-result Rock Paper = 0
-result Rock Rock = 3
-result Scissors Scissors = 3
-result Paper Paper = 3
-result Scissors Rock = 0
-result Scissors Paper = 6
-result Paper Rock = 6
-result Paper Scissors = 0
-
+-- defining an alternate ordering that loops around
+-- A > B means that A beats B
+sCompare :: Shape -> Shape -> Ordering
+sCompare Scissors Rock = LT
+sCompare Rock Scissors = GT
+sCompare a b = compare a b
+-- defaults to compare when not needing to wrap
+-- defining an operator for ease of use
+(?=) = sCompare
 
 
 
 
 
 
+-- points given by choice of move
+selectScore :: Shape -> Int
+selectScore Rock = 1
+selectScore Paper = 2
+selectScore Scissors = 3
 
-select :: Shape -> Int
-select Rock = 1
-select Paper = 2
-select Scissors = 3
+-- points given by result of match
+outcomeScore :: Ordering -> Int
+outcomeScore LT = 0
+outcomeScore EQ = 3
+outcomeScore GT = 6
 
-
+-- converting from the guide to a choice of moves
 toShape :: Char -> Shape
 toShape 'A' = Rock
-toShape 'X' = Rock
 toShape 'B' = Paper
-toShape 'Y' = Paper
 toShape 'C' = Scissors
+
+toShape 'X' = Rock
+toShape 'Y' = Paper
 toShape 'Z' = Scissors 
 
-roundPoints :: String -> Int
-roundPoints info = (select choice) + (result choice opp)
-    where opp = toShape (head info)
-          choice = toShape (last info)
+
+-- total points for one single game, following the first guide
+-- just adds points for move and points for outcome
+gamePoints :: String -> Int
+gamePoints game = selectScore choice + outcomeScore (choice ?= opp)
+    where opp = toShape (head game)
+          choice = toShape (last game)
           
+-- rule for following the second guide
+toOutcome :: Char -> Ordering
+toOutcome 'X' = LT
+toOutcome 'Y' = EQ
+toOutcome 'Z' = GT
           
-losesTo :: Shape -> Shape
-losesTo Rock = Scissors
-losesTo Scissors = Paper
-losesTo Paper = Rock
-  
-
-beats :: Shape -> Shape
-beats Rock = Paper
-beats Scissors = Rock
-beats Paper = Scissors
-
-ties :: Shape -> Shape
-ties s = s
-
-rule :: Char -> (Shape -> Shape)
-rule 'X' = losesTo
-rule 'Y' = ties
-rule 'Z' = beats
-
+-- we don't actually need to figure out our move
+-- we just need the points for the outcome
+-- and to shift the points for selection up or down a bit
 riggedPoints :: String -> Int
-riggedPoints info = (select choice) + (result choice opp)
-    where opp = toShape (head info)
-          choice = rule (last info) $ opp
+riggedPoints game 
+    | outcome == LT = 0 + (oppPoints - 2) `mod` 3 + 1
+    | outcome == EQ = 3 + (oppPoints)
+    | outcome == GT = 6 + (oppPoints) `mod` 3 + 1
+    where opp       = toShape (head game)
+          outcome   = toOutcome (last game)
+          oppPoints = selectScore opp
+          
           
 
 
@@ -73,7 +74,7 @@ solve = do
     contents <- readFile "input/Day2.txt"
     
     let rounds = lines contents
-        normalScore = sum $ map roundPoints  rounds
+        normalScore = sum $ map gamePoints  rounds
         riggedScore = sum $ map riggedPoints rounds
     
     putStrLn $ "First  Solution:\t" ++ show normalScore
