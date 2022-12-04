c987630a0ee6260e5f996f240fc8cf83037c8593
ivy bridges <ivy.v.bridges@gmail.com>
2022-12-04 01:06:35 -0500

    added comments and rewrote splitting function

--- solutions/Day4.hs
+++ solutions/Day4.hs
@@ -3,16 +3,21 @@ module Day4 where
 import Data.List
 import Data.Maybe
 
-splitAtComma :: String -> (String, String)
-splitAtComma str = (firstAssignment, secondAssignment)
-    where firstAssignment = fst $ break (==',') str
-          secondAssignment = tail $ snd $ break (==',') str
+
+
+-- splits a string at a character, then removes the character from the second item
+splitAtChar :: Char -> String -> (String, String)
+splitAtChar chr str = (firstItem, secondItem)
+    where firstItem = fst $ break (==chr) str
+          secondItem = tail $ snd $ break (==chr) str
           
+-- takes an elf's section assignment and returns the starting and ending sections  
+-- sections are split at a - character        
 toRange :: String -> (Int, Int)
-toRange assignment = (start, end)
-    where start = read $ fst $ break (=='-') assignment
-          end   = read $ tail $ snd $ break (=='-') assignment
-          
+toRange assignment = (read start, read end)
+    where (start, end) = splitAtChar '-' assignment
+
+-- given a pair of ranges, test whether one contaisn the other          
 testContains :: (String, String) -> Bool
 testContains (firstAssignment, secondAssignment)
     | first < second = (firstEnd >= secondEnd)
@@ -20,7 +25,8 @@ testContains (firstAssignment, secondAssignment)
     | otherwise = True
     where (first, firstEnd) = toRange firstAssignment
           (second, secondEnd) = toRange secondAssignment
-          
+
+-- given a pair of ranges, test whether one overlaps with the other         
 testOverlap :: (String, String) -> Bool
 testOverlap (firstAssignment, secondAssignment)
     | first < second = (firstEnd >= second)
@@ -36,9 +42,13 @@ solve = do
     input <- readFile "input/day4.txt"
     
     let pairs = lines input
-        parsedPairs = map (splitAtComma) pairs
+        -- pairs are split at a , character
+        parsedPairs = map (splitAtChar ',') pairs
         
+        -- first puzzle is to count the number of pairs where one contins the other
         containedPairs = filter (testContains) parsedPairs
+        
+        -- second puzzle is to count the number of pairs where one overlaps with the other
         overlapPairs   = filter (testOverlap)  parsedPairs
     
     
